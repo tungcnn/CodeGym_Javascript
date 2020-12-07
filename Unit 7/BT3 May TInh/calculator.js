c4f9ef867ed0365b29ef3701e3f1deb720b03985
tungcnn <sontung.cnn@gmail.com>
2020-12-07 21:54:13 +0700

    Add many functions to calculator.js

--- Unit 7/BT3 May TInh/calculator.js
+++ Unit 7/BT3 May TInh/calculator.js
@@ -1,7 +1,74 @@
+let _calculated = false;
 function display(val) {
-    document.getElementById("expression").value += val;
+    if (_calculated) {
+        CE();
+        document.getElementById("expression").value += val;
+    } else {
+        document.getElementById("expression").value += val;
+    } 
 }
 function calculate() {
-    let result = eval(document.getElementById("expression").value);
+    let currentExpression = document.getElementById("expression").value;
+    document.getElementById("previousexpression").value = currentExpression;
+
+    let result = +eval(currentExpression).toFixed(4);
     document.getElementById("expression").value = result;
+    _calculated = true;
+}
+function CE() {
+    let currentExpression = document.getElementById("expression").value;
+    if (_calculated) {
+        document.getElementById("previousexpression").value = currentExpression;
+        document.getElementById('expression').value = '';
+    } else {
+        currentExpression = currentExpression.slice(0, -1);
+        document.getElementById('expression').value = currentExpression;
+    }
+    _calculated = false;
+}
+function askInput() {
+    let x = prompt("Enter your number:");
+    return x;
+}
+function squareRoot() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.sqrt(x).toFixed(4);
+}
+function power() {
+    let x = askInput();
+    let y = askInput();
+    document.getElementById('expression').value += +Math.pow(x, y).toFixed(4);    
+}
+function ln() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.log(x).toFixed(4);    
+}
+function log() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.log10(x).toFixed(4);
+}
+function sin() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.sin(x).toFixed(4);
+}
+function cos() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.cos(x).toFixed(4);
+}
+function tan() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.tan(x).toFixed(4);
+}
+function exp() {
+    let x = askInput();
+    document.getElementById('expression').value += +Math.exp(x).toFixed(4);
+}
+function ans() {
+    let previousexpression = document.getElementById('previousexpression').value;
+    if (previousexpression != null) {
+        document.getElementById('expression').value += previousexpression;
+    }
+}
+function saySomething() {
+    alert("This button is useless");
 }
